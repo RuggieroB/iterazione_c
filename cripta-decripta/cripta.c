073a037a21d95c89d30a99d82f863d410b56cb77
EnricoRuggieroB <1bmeccenricorugbucchianico@gmail.com>
2019-03-23 11:28:53 +0000

    Algoritmi di Laboratorio di Programmazione. Alunno: Bucchianico Enrico Ruggiero, 4^Finf.

---
+++ cripta-decripta/cripta.c
@@ -0,0 +1,47 @@
+/*
+# iterazione_c
+
+# Algoritmi di Laboratorio di Programmazione. Alunno: Bucchianico Enrico Ruggiero, 4^Finf.
+
+
+- Cartella "cripta-decripta":
+	- Algoritmo "cripta.c": Algoritmo che accetta un intero di quattro cifre. Sostituisce ogni cifra con il risultato della somma modulo dieci tra la cifra stessa e sette. 
+				 Infine scambia la prima cifra con la terza e la seconda con la quarta.
+
+	- Algoritmo "decripta.c": Algoritmo che accetta un intero di quattro cifre crittografato con il metodo dell'algoritmo "cripta.c" (Accetta un intero di quattro cifre. 
+				   Sostituisce ogni cifra con il risultato della somma modulo dieci tra la cifra stessa e sette. Infine scambia la prima cifra con la terza e 
+				    la seconda con la quarta.) e restituisce il numero originale.
+*/
+
+#include <stdio.h>
+int main()
+{
+	int num,c1,c2,c3,c4,temp;
+	do {
+		printf("Inserire un intero di 4 cifre:\t");
+		scanf("%d",&num);
+		putchar('\n');
+	} while ( num <= 100 || num > 9999 );
+	printf("\n Numero da crittografare:\t%d",num);                                                                     
+	putchar('\n');
+	c1=num%10;
+	c1=(c1+7)%10;
+	num/=10;
+	c2=num%10;
+	c2=(c2+7)%10;
+	num/=10;
+	c3=num%10;
+	c3=(c3+7)%10;
+	num/=10;
+	c4=num%10;
+	c4=(c4+7)%10;
+	temp=c1;
+	c1=c3;
+	c3=temp;
+	temp=c2;
+	c2=c4;
+	c4=temp;
+	num = c4*1000+c3*100+c2*10+c1;
+	printf("\n\nNumero crittografato:\t%d\n",num);
+	return 0;
+}
